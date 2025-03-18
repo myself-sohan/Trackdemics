15f31334d3c51aa5e01111c1daeba07c42aff40b
myself-sohan <sohansaha.11sc.2020@gmail.com>
2025-03-18 11:06:19 +0530

    Added Material Theme Builder

--- app/src/main/java/com/example/trackdemics/MainActivity.kt
+++ app/src/main/java/com/example/trackdemics/MainActivity.kt
@@ -4,6 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.trackdemics.navigation.TrackdemicsNavigation
 import com.example.trackdemics.ui.theme.TrackdemicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +21,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TrackdemicsTheme {
-
+                TrackdemicsApp()
             }
         }
     }
+}
+
+@Composable
+fun TrackdemicsApp()
+{
+    Surface(color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize(), content = {
+            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                TrackdemicsNavigation()
+
+            }
+        })
 }
--- app/src/main/java/com/example/trackdemics/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/trackdemics/screens/login/LoginScreen.kt
@@ -1,5 +1,11 @@
 package com.example.trackdemics.screens.login
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
@@ -8,5 +14,18 @@ fun LoginScreen(
     navController: NavController
 )
 {
-
+    Surface(
+        color = MaterialTheme.colorScheme.background
+    )
+    {
+        Column(
+            verticalArrangement = Arrangement.Center
+        )
+        {
+            Text(
+                text = "Login Screen",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
 }
