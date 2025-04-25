02929bc51d0a0b14d565cf5332f89c31d3744743
Ashad <ashadcancode@gmail.com>
2025-04-25 16:16:41 +0530

    Minor change

--- app/src/main/java/com/example/trackdemics/screens/home/AdminHomeScreen.kt
+++ app/src/main/java/com/example/trackdemics/screens/home/AdminHomeScreen.kt
@@ -11,38 +11,32 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Assignment
 import androidx.compose.material.icons.automirrored.filled.MenuBook
-import androidx.compose.material.icons.filled.Event
 import androidx.compose.material.icons.filled.EventAvailable
 import androidx.compose.material.icons.filled.Grade
 import androidx.compose.material.icons.filled.Groups
-import androidx.compose.material.icons.filled.Lightbulb
 import androidx.compose.material.icons.filled.ManageAccounts
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Schedule
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.trackdemics.screens.home.components.FeatureCard
 import com.example.trackdemics.screens.home.components.ProfileSection
 import com.example.trackdemics.screens.home.components.SideNavigationPanel
 import com.example.trackdemics.screens.home.model.FeatureItem
-import com.example.trackdemics.widgets.WelcomeText
+import com.example.trackdemics.screens.signup.SignUpViewModel
 import com.example.trackdemics.widgets.TrackdemicsAppBar
 
 @Composable
 fun AdminHomeScreen(
+    signUpViewModel: SignUpViewModel = hiltViewModel(),
     navController: NavController,
 )
 {
@@ -57,7 +51,7 @@ fun AdminHomeScreen(
                     .background(color = MaterialTheme.colorScheme.background)
             )
             {
-                SideNavigationPanel()
+                SideNavigationPanel(signUpViewModel, navController)
             }
         },
         scrimColor = MaterialTheme.colorScheme.background
